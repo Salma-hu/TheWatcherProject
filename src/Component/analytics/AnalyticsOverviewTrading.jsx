1cff63d50cfbb36b0078402dafab5c4fc904fa3f
Salma-hu <salmabelassiri@gmail.com>
2025-02-12 16:45:34 +0100

    Update AnalyticsOverviewTrading.jsx

--- src/Component/analytics/AnalyticsOverviewTrading.jsx
+++ src/Component/analytics/AnalyticsOverviewTrading.jsx
@@ -5,23 +5,25 @@ import NavButtons from "@/Component/analytics/NavButtons";
 import TradeSummaryCard from "@/Component/analytics/TradeSummaryCard";
 import PnLChart from "@/Component/analytics/PnLChart";
 
+
 const AnalyticsOverviewTrading = () => {
   const { theme } = useTheme();
-
+  
   return (
     <div className="p-4">
       <div className="grid gap-4">
-        {/* Stats Cards and Navigation Buttons */}
         <div className="flex justify-between items-center">
-          {/* Trades widget on the far left */}
           <TradeSummaryCard />
-          {/* Navigation buttons on the far right */}
           <NavButtons />
         </div>
-
-        <div>
-          <PnLChart />a
-        </div>
+        <div className="grid grid-cols-4 gap-4">
+            <div className="col-span-1 row-span-3">
+              {/* <AverageTimeList /> */}
+            </div>
+            <div className="col-span-3 flex flex-col gap-4">
+              <PnLChart />
+            </div>
+          </div>
       </div>
     </div>
   );
